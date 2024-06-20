4e95ba84a41cf686a211a51659f39ad0cd8f3024
TamNgne <nguyenthithanhtam1305@gmail.com>
2024-06-20 07:41:39 +0700

    upload database

--- lib/data/skincaredatabase.sql
+++ lib/data/skincaredatabase.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `skincare` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `skincare`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: skincare
@@ -59,4 +61,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-20  7:16:15
+-- Dump completed on 2024-06-20  7:31:53
